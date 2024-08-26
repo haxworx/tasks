01aafc4eb7ed83d9085d4309f31d2713945828e4
Alastair Poole <mail@alastairpoole.com>
2024-08-26 16:54:17 +0100

    controllers: rename

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IndexController extends AbstractController
+class PageController extends AbstractController
 {
     #[Route('/', name: 'index')]
     public function index(): Response
